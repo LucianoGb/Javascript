0dc2f041ae55d644923d50ac3ee63ad47d471ddf
Luciano Gouveia <ljdgb2012@gmail.com>
2021-02-10 15:11:30 -0300

    atualiza aplicação

---
+++ maratona-discovery/script.js
@@ -0,0 +1,10 @@
+let Modal = {
+    open(){
+        document.querySelector('.modal-overlay').classList.add('active')
+    },
+
+    close(){
+        document.querySelector('.modal-overlay').classList.remove('active')
+
+    }
+}
